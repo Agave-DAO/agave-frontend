8c6f785e842f2650d15acf1f28c2ad22229fc323
zedkai <zedkai@zedk.ai>
2021-04-30 04:41:36 -0700

    Updated to new lending pool contract location

--- src/utils/contracts/contractAddresses/internalAddresses.ts
+++ src/utils/contracts/contractAddresses/internalAddresses.ts
@@ -1,5 +1,5 @@
 export const internalAddresses = {
-    Lending: '0xb18bc34dc9705421633926ae766DEa38cA22dD94',
+    Lending: '0xeBEb4Ff34423eA229967D56D7AE1dB270e0cAc8F',
     assets : {
         ETH: '0x4eB1d08A6c8F907b90c3770F981E13240E4c419A',
         DAI: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea'
