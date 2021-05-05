d97a724b5f5c82492e9cdbc3dc26015feee83564
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-05-05 16:14:08 +0530

    chore: remove material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -24,102 +24,46 @@ import WithdrawConfirm from "./views/Withdraw/WithdrawConfirm";
 // import InterestSwap from './views/InterestSwap';
 import { Staking } from "./views/Staking";
 import "react-notifications-component/dist/theme.css";
-import BaseTheme from "./theme";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 //import "animate.css/animate.min.css";
 import "react-notifications-component/dist/theme.css";
 import ReactNotification from "react-notifications-component";
 import { useReduxWeb3Updater } from "./hooks/reduxWeb3";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 interface IAppProps {}
 
 const App: React.FC<IAppProps> = (props) => {
   const notifications = React.useMemo(() => <ReactNotification />, []);
-  // TODO: Proper theming interop a la https://material-ui.com/guides/interoperability/#styled-components
-  const appTheme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          primary: {
-            main: "#019d8b",
-            light: "#eefef7",
-            dark: "#007c6e",
-          },
-          secondary: {
-            main: "#007c6e",
-            light: "#019d8b",
-            dark: "#044D44",
-          },
-          text: {
-            // primary: "#00cdB9",
-            // secondary: "#ffffff",
-            hint: "red",
-          },
-          // type: "dark",
-          // primary: {
-          //   main: "#007c6e",
-          //   light: "#019d8b",
-          //   dark: "#044D44",
-          //   contrastText: "purple"
-          // },
-          // secondary: {
-          //   main: "#dcfff1",
-          //   light: "#F3FFF7",
-          //   dark: "#044D44",
-          // },
-          // text: {
-          //   primary: "#ffffff",
-          //   // secondary: "#007c6e",
-          //   secondary: "#00cdB9",
-          //   hint: "red"
-          // },
-          // tonalOffset: {
-          //   dark: 0,
-          //   light: 0.5,
-          // },
-          // background: {
-          //   paper: "#007c6e",
-          //   default: "#044D44",
-          // }
-        },
-      }),
-    []
-  );
+
   useReduxWeb3Updater();
   return (
-    <MuiThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <ThemeProvider theme={BaseTheme}>
-        {notifications}
-        <Router>
-          <Layout>
-            {/* prettier-ignore */}
-            <Switch>
-              <Route path="/markets" component={Markets} exact />
-              <Route path="/deposit" component={Deposit} exact />
-              <Route path="/deposit/:assetName" component={DepositDetail} exact />
-              <Route path="/deposit/confirm/:assetName/:amount" component={DepositConfirm} exact />
-              <Route path="/borrow" component={Borrow} exact />
-              <Route path="/borrow/:assetName" component={BorrowDetail} exact />
-              <Route path="/borrow/confirm/:assetName/:amount" component={BorrowConfirm} exact />
-              <Route path="/withdraw/:assetName" component={WithdrawDetail} exact />
-              <Route path="/withdraw/confirm/:assetName/:amount" component={WithdrawConfirm} exact />
-              <Route path="/stake" component={Staking} exact />
-              {/* <Route path="/reserve-overview/:assetName" component={ReserveOverview} exact /> */}
-              {/* <Route path="/dashboard" component={Dashboard} exact /> */}
-              {/* <Route path="/repay/:assetName" component={RepayDetail} exact /> */}
-              {/* <Route path="/repay/confirm/:assetName/:amount" component={RepayConfirm} exact /> */}
-              {/* <Route path="/collateral/:assetName" component={Collateral} exact /> */}
-              {/* <Route path="/interest-swap/:assetName" component={InterestSwap} exact /> */}
-              <Redirect from="/" to="/markets" />
-            </Switch>
-          </Layout>
-        </Router>
-      </ThemeProvider>
-    </MuiThemeProvider>
+    <>
+      {notifications}
+      <Router>
+        <Layout>
+          {/* prettier-ignore */}
+          <Switch>
+            <Route path="/markets" component={Markets} exact />
+            <Route path="/deposit" component={Deposit} exact />
+            <Route path="/deposit/:assetName" component={DepositDetail} exact />
+            <Route path="/deposit/confirm/:assetName/:amount" component={DepositConfirm} exact />
+            <Route path="/borrow" component={Borrow} exact />
+            <Route path="/borrow/:assetName" component={BorrowDetail} exact />
+            <Route path="/borrow/confirm/:assetName/:amount" component={BorrowConfirm} exact />
+            <Route path="/withdraw/:assetName" component={WithdrawDetail} exact />
+            <Route path="/withdraw/confirm/:assetName/:amount" component={WithdrawConfirm} exact />
+            <Route path="/stake" component={Staking} exact />
+            {/* <Route path="/reserve-overview/:assetName" component={ReserveOverview} exact /> */}
+            {/* <Route path="/dashboard" component={Dashboard} exact /> */}
+            {/* <Route path="/repay/:assetName" component={RepayDetail} exact /> */}
+            {/* <Route path="/repay/confirm/:assetName/:amount" component={RepayConfirm} exact /> */}
+            {/* <Route path="/collateral/:assetName" component={Collateral} exact /> */}
+            {/* <Route path="/interest-swap/:assetName" component={InterestSwap} exact /> */}
+            <Redirect from="/" to="/markets" />
+          </Switch>
+        </Layout>
+      </Router>
+    </>
   );
 };
 
