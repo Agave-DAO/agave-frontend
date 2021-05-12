27da5f62020c2e7833942f4d6330322e46372c98
zedkai <zedkai@zedk.ai>
2021-05-11 19:39:47 -0700

    Fix ambient connection reopening

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import reportWebVitals from './reportWebVitals';
-import type { AbstractConnector } from '@web3-react/abstract-connector';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
+import type { AbstractConnector } from "@web3-react/abstract-connector";
 import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from '@ethersproject/providers'
-import {
-  QueryClient,
-  QueryClientProvider,
-  QueryCache,
-} from "react-query";
-import { BigNumber, FixedNumber } from 'ethers';
-import { useAmbientConnection } from './hooks/injectedConnectors';
+import { Web3Provider } from "@ethersproject/providers";
+import { QueryClient, QueryClientProvider, QueryCache } from "react-query";
+import { BigNumber, FixedNumber } from "ethers";
+import { useAmbientConnection } from "./hooks/injectedConnectors";
 
 const reactQueryClient = new QueryClient({
   queryCache: new QueryCache(),
 });
 
-function getWeb3Library(provider: any, connector?: AbstractConnector | undefined): Web3Provider {
+function getWeb3Library(
+  provider: any,
+  connector?: AbstractConnector | undefined
+): Web3Provider {
   const library = new Web3Provider(provider);
   library.pollingInterval = 12_000;
   return library;
@@ -28,22 +27,22 @@ function getWeb3Library(provider: any, connector?: AbstractConnector | undefined
 
 const AppRoot: React.FC<{}> = () => {
   useAmbientConnection();
-  return (
+  return <App />;
+};
+
+// function wrapper is used to enable HMR
+function renderApp() {
+  ReactDOM.render(
     <Web3ReactProvider getLibrary={getWeb3Library}>
       <Provider store={store}>
         <QueryClientProvider client={reactQueryClient}>
           <React.StrictMode>
-            <App />
+            <AppRoot />
           </React.StrictMode>
         </QueryClientProvider>
       </Provider>
-    </Web3ReactProvider>
-  );
-}
-
-// function wrapper is used to enable HMR
-function renderApp() {
-  ReactDOM.render(<AppRoot/>, document.getElementById("root")
+    </Web3ReactProvider>,
+    document.getElementById("root")
   );
 }
 
@@ -54,9 +53,8 @@ renderApp();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-
 if (process.env.NODE_ENV === "development" && module.hot) {
   (window as any).FixedNumber = FixedNumber;
   (window as any).BigNumber = BigNumber;
-  module.hot.accept("./App", renderApp)
+  module.hot.accept("./App", renderApp);
 }
