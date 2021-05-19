3728c998f526bc3d6ec60e8bf46feba657afcead
zedkai <zedkai@zedk.ai>
2021-05-19 04:36:38 -0700

    Added Rinkeby addresses for contracts used by the Markets queries

--- src/utils/contracts/contractAddresses/internalAddresses.ts
+++ src/utils/contracts/contractAddresses/internalAddresses.ts
@@ -3,6 +3,10 @@ export interface StrongTypedChainAddresses<Id extends ValidNetworkIdTypes, Name 
   readonly chainName: Name;
   readonly lendingPool: string;
   readonly staking: string;
+
+  readonly agaveOracle: string;
+  readonly aaveProtocolDataProvider: string;
+  readonly wrappedNativeGateway: string;
 }
 
 export interface ChainAddresses extends StrongTypedChainAddresses<any, any> {
@@ -31,12 +35,20 @@ export const internalAddressesPerNetwork: Record<
     chainId: 4,
     lendingPool: "0xeBEb4Ff34423eA229967D56D7AE1dB270e0cAc8F",
     staking: "0xdefd31e8c8e5e7db1d2e2204c99d006e1607554b",
+
+    agaveOracle: "0xb42a48F27150469d8a55dA3ad258B73a37239451",
+    aaveProtocolDataProvider: "0x0223f6FE9c7d4AA52C8C1877efEB46DcabC5270B",
+    wrappedNativeGateway: "0x90Df46541E7a42e85E57CA92C29F0c8338c88BDd",
   },
   xdai: {
     chainName: "xdai",
     chainId: 100,
     lendingPool: "0x207E9def17B4bd1045F5Af2C651c081F9FDb0842",
     staking: "0x610525b415c1BFAeAB1a3fc3d85D87b92f048221",
+
+    agaveOracle: "0xACABACABACABACABACABACABACABACABACABACAB",
+    aaveProtocolDataProvider: "0xACABACABACABACABACABACABACABACABACABACAB",
+    wrappedNativeGateway: "0xACABACABACABACABACABACABACABACABACABACAB",
   },
 } as const;
 
