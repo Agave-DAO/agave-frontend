714d729275c71a4032c624cd82a43a45ee144d6b
zedkai <zedkai@zedk.ai>
2021-05-20 02:35:14 -0700

    Added 3 more query implementations

---
+++ src/queries/allReserveTokens.ts
@@ -0,0 +1,24 @@
+import { AaveProtocolDataProvider__factory } from "../contracts";
+import { buildQueryHookWhenParamsDefinedChainAddrs } from "../utils/queryBuilder";
+
+export interface ReserveTokenDefinition {
+  readonly symbol: string;
+  readonly tokenAddress: string;
+}
+
+export const useAllReserveTokens =
+  buildQueryHookWhenParamsDefinedChainAddrs<
+    ReadonlyArray<ReserveTokenDefinition>,
+    [_p1: "AaveProtocolDataProvider", _p2: "getAllReserveTokens"],
+    []
+  >(
+    async params => {
+      const contract = AaveProtocolDataProvider__factory.connect(
+        params.chainAddrs.aaveProtocolDataProvider,
+        params.library.getSigner()
+      );
+      return await contract.getAllATokens();
+    },
+    () => ["AaveProtocolDataProvider", "getAllReserveTokens"],
+    () => undefined
+  );
