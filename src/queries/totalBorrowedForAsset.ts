2a85f5478612edf089c9ca99e91c40cb6ee65a1b
zedkai <zedkai@zedk.ai>
2021-05-23 21:56:53 -0700

    Implement totalBorrowedForAsset query

---
+++ src/queries/totalBorrowedForAsset.ts
@@ -0,0 +1,41 @@
+import { BigNumber, FixedNumber } from "@ethersproject/bignumber";
+import { constants } from "ethers";
+import { buildQueryHookWhenParamsDefinedChainAddrs } from "../utils/queryBuilder";
+import { useAssetPriceInDaiWei } from "./assetPriceInDai";
+import { useDecimalCountForToken, weiPerToken } from "./decimalsForToken";
+import { useProtocolReserveData } from "./protocolReserveData";
+
+export const useTotalBorrowedForAsset =
+  buildQueryHookWhenParamsDefinedChainAddrs<
+    { wei: BigNumber; dai: FixedNumber },
+    [_p1: "asset", assetAddress: string | undefined, _p2: "borrowed"],
+    [assetAddress: string]
+  >(
+    async (params, assetAddress) => {
+      const [reserveData, assetPriceInDaiWei, assetDecimals] =
+        await Promise.all([
+          useProtocolReserveData.fetchQueryDefined(params, assetAddress),
+          useAssetPriceInDaiWei.fetchQueryDefined(params, assetAddress),
+          useDecimalCountForToken.fetchQueryDefined(params, assetAddress),
+        ]);
+
+      const totalBorrowedWei = reserveData.totalStableDebt.add(
+        reserveData.totalVariableDebt
+      );
+
+      const daiBorrowed = FixedNumber.fromValue(
+        totalBorrowedWei
+          .mul(assetPriceInDaiWei)
+          .mul(constants.WeiPerEther)
+          .div(weiPerToken(assetDecimals)),
+        18
+      );
+
+      return {
+        wei: totalBorrowedWei,
+        dai: daiBorrowed,
+      };
+    },
+    assetAddress => ["asset", assetAddress, "borrowed"],
+    () => undefined
+  );
