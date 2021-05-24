2c3ded8f5ab1711330cc51a7bafab26f7f301147
zedkai <zedkai@zedk.ai>
2021-05-23 21:53:16 -0700

    Added a query for decimals per token, so we can cache it

---
+++ src/queries/decimalsForToken.ts
@@ -0,0 +1,28 @@
+import { BigNumber, BigNumberish, FixedNumber } from "@ethersproject/bignumber";
+import { Erc20abi__factory } from "../contracts";
+import { buildQueryHookWhenParamsDefinedChainAddrs } from "../utils/queryBuilder";
+
+export function weiPerToken(decimals: BigNumberish): BigNumber {
+  return BigNumber.from(10).pow(decimals);
+}
+
+export const useDecimalCountForToken =
+  buildQueryHookWhenParamsDefinedChainAddrs<
+    number,
+    [_p1: "erc20", assetAddress: string | undefined, _p2: "decimals"],
+    [assetAddress: string]
+  >(
+    async (params, assetAddress) => {
+      const contract = Erc20abi__factory.connect(
+        assetAddress,
+        params.library.getSigner()
+      );
+      return contract.decimals();
+    },
+    assetAddress => ["erc20", assetAddress, "decimals"],
+    () => undefined,
+    {
+      staleTime: 60 * 60 * 24 * 1000,
+      cacheTime: 60 * 60 * 24 * 1000,
+    }
+  );
