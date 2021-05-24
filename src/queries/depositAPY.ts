5ee786d9cd8b638a74f7c2bb90ef0f030f5d8f83
zedkai <zedkai@zedk.ai>
2021-05-23 22:48:34 -0700

    Implemented stable and variable borrow APRs
    Also fixed cache target for depositAPY to not overlap utilizationRate

--- src/queries/depositAPY.ts
+++ src/queries/depositAPY.ts
@@ -4,7 +4,7 @@ import { useProtocolReserveData } from "./protocolReserveData";
 
 export const useDepositAPY = buildQueryHookWhenParamsDefinedChainAddrs<
   FixedNumber,
-  [_p1: "asset", assetAddress: string | undefined, _p2: "utilizationRate"],
+  [_p1: "asset", assetAddress: string | undefined, _p2: "depositAPY"],
   [assetAddress: string]
 >(
   async (params, assetAddress) => {
@@ -14,7 +14,45 @@ export const useDepositAPY = buildQueryHookWhenParamsDefinedChainAddrs<
     );
     return liquidityRate;
   },
-  assetAddress => ["asset", assetAddress, "utilizationRate"],
+  assetAddress => ["asset", assetAddress, "depositAPY"],
+  () => undefined,
+  {
+    staleTime: 5 * 1000,
+    cacheTime: 30 * 1000,
+  }
+);
+
+export const useVariableBorrowAPR = buildQueryHookWhenParamsDefinedChainAddrs<
+  FixedNumber,
+  [_p1: "asset", assetAddress: string | undefined, _p2: "variableBorrowAPR"],
+  [assetAddress: string]
+>(
+  async (params, assetAddress) => {
+    const { variableBorrowRate } =
+      await useProtocolReserveData.fetchQueryDefined(params, assetAddress);
+    return variableBorrowRate;
+  },
+  assetAddress => ["asset", assetAddress, "variableBorrowAPR"],
+  () => undefined,
+  {
+    staleTime: 5 * 1000,
+    cacheTime: 30 * 1000,
+  }
+);
+
+export const useStableBorrowAPR = buildQueryHookWhenParamsDefinedChainAddrs<
+  FixedNumber,
+  [_p1: "asset", assetAddress: string | undefined, _p2: "stableBorrowAPR"],
+  [assetAddress: string]
+>(
+  async (params, assetAddress) => {
+    const { stableBorrowRate } = await useProtocolReserveData.fetchQueryDefined(
+      params,
+      assetAddress
+    );
+    return stableBorrowRate;
+  },
+  assetAddress => ["asset", assetAddress, "stableBorrowAPR"],
   () => undefined,
   {
     staleTime: 5 * 1000,
