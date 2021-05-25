23e4a5a0a885ba099551fbd8a7b95ea36b8cea0e
zedkai <zedkai@zedk.ai>
2021-05-25 01:23:22 -0700

    Implemented user deposited assets and USD value of deposited assets queries

--- src/queries/userAssets.ts
+++ src/queries/userAssets.ts
@@ -1,6 +1,7 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { Erc20abi__factory } from "../contracts";
 import { buildQueryHookWhenParamsDefinedChainAddrs } from "../utils/queryBuilder";
+import { useAllATokens } from "./allATokens";
 import { useAllReserveTokens } from "./allReserveTokens";
 import { useAssetPriceInDaiWei } from "./assetPriceInDai";
 import { useDecimalCountForToken, weiPerToken } from "./decimalsForToken";
@@ -54,15 +55,18 @@ export const useUserReserveAssetBalances =
     }
   );
 
-export const useUserReserveAssetBalancesDai =
+interface ReserveAssetBalancesDaiWei {
+  symbol: string;
+  tokenAddress: string;
+  balance: BigNumber;
+  decimals: BigNumberish;
+  daiWeiPricePer: BigNumber;
+  daiWeiPriceTotal: BigNumber;
+}
+
+export const useUserReserveAssetBalancesDaiWei =
   buildQueryHookWhenParamsDefinedChainAddrs<
-    {
-      symbol: string;
-      tokenAddress: string;
-      balance: BigNumber;
-      daiWeiPricePer: BigNumber;
-      daiWeiPriceTotal: BigNumber;
-    }[],
+    ReserveAssetBalancesDaiWei[],
     [_p1: "user", _p2: "allReserves", _p3: "balances", _p4: "dai"],
     []
   >(
@@ -87,6 +91,7 @@ export const useUserReserveAssetBalancesDai =
             daiWeiPriceTotal: daiPricePerToken
               .mul(reserve.balance)
               .div(weiPerToken(decimals)),
+            decimals,
           }))
         )
       );
@@ -94,6 +99,102 @@ export const useUserReserveAssetBalancesDai =
       return withDaiPrices;
     },
     () => ["user", "allReserves", "balances", "dai"],
+    () => undefined,
+    {
+      refetchOnMount: true,
+      staleTime: 2 * 60 * 1000,
+      cacheTime: 60 * 60 * 1000,
+    }
+  );
+
+export const useUserDepositAssetBalances =
+  buildQueryHookWhenParamsDefinedChainAddrs<
+    { symbol: string; tokenAddress: string; balance: BigNumber }[],
+    [_p1: "user", _p2: "allDeposits", _p3: "balances"],
+    []
+  >(
+    async params => {
+      const aTokens = await useAllATokens.fetchQueryDefined(params);
+
+      const aTokensWithBalances = await Promise.all(
+        aTokens.map(reserve =>
+          useUserAssetBalance
+            .fetchQueryDefined(params, reserve.tokenAddress)
+            .then(result => ({ ...reserve, balance: result }))
+        )
+      );
+
+      return aTokensWithBalances;
+    },
+    () => ["user", "allDeposits", "balances"],
+    () => undefined,
+    {
+      refetchOnMount: true,
+      staleTime: 2 * 60 * 1000,
+      cacheTime: 60 * 60 * 1000,
+    }
+  );
+
+interface DepositAssetBalancesDaiWei {
+  symbol: string;
+  aSymbol: string;
+  tokenAddress: string;
+  aTokenAddress: string;
+  balance: BigNumber;
+  daiWeiPricePer: BigNumber;
+  daiWeiPriceTotal: BigNumber;
+}
+
+export const useUserDepositAssetBalancesDaiWei =
+  buildQueryHookWhenParamsDefinedChainAddrs<
+    DepositAssetBalancesDaiWei[],
+    [_p1: "user", _p2: "allDeposits", _p3: "balances", _p4: "dai"],
+    []
+  >(
+    async params => {
+      const [aTokens, reserves] = await Promise.all([
+        useAllATokens.fetchQueryDefined(params).then(result =>
+          Promise.all(
+            result.map(aToken =>
+              useUserAssetBalance
+                .fetchQueryDefined(params, aToken.tokenAddress)
+                .then(aTokenBalance => ({
+                  ...aToken,
+                  balance: aTokenBalance,
+                }))
+            )
+          )
+        ),
+        useUserReserveAssetBalancesDaiWei.fetchQueryDefined(params),
+      ]);
+
+      const reservesByTokenAddr = Object.fromEntries(
+        reserves.map(r => [r.tokenAddress, r])
+      );
+
+      const withDaiPrices: DepositAssetBalancesDaiWei[] = [];
+      for (const at of aTokens) {
+        const reserve = reservesByTokenAddr[at.tokenAddress];
+        if (!reserve) {
+          console.warn("Equivalent reserve not present for aToken:", at);
+          continue;
+        }
+        withDaiPrices.push({
+          aSymbol: at.symbol,
+          symbol: reserve.symbol,
+          aTokenAddress: at.tokenAddress,
+          tokenAddress: reserve.tokenAddress,
+          balance: at.balance,
+          daiWeiPricePer: reserve.daiWeiPricePer,
+          daiWeiPriceTotal: at.balance
+            .mul(reserve.daiWeiPricePer)
+            .div(weiPerToken(reserve.decimals)),
+        });
+      }
+
+      return withDaiPrices;
+    },
+    () => ["user", "allDeposits", "balances", "dai"],
     () => undefined,
     {
       refetchOnMount: true,
