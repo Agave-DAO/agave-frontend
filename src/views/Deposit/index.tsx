90c13ef2e29a8abfa9a123374d514c65da87a0cf
Fabian Molina <famole@gmail.com>
2021-06-10 23:00:18 -0300

    Added deposits data logic

--- src/views/Deposit/index.tsx
+++ src/views/Deposit/index.tsx
@@ -1,17 +1,37 @@
 import React, { useState } from 'react';
+import { BigNumber } from 'ethers';
+import { useUserDepositAssetBalances } from  '../../queries/userAssets'
 import {DepositLayout} from './layout';
+
+export interface DepositAsset {
+  symbol: string,
+  tokenAddress: string,
+  balance: BigNumber
+}
 
 function Deposit() {
   const [activeValue, setActiveValue] = useState<"All" | "Stable Coins">('All');
+  const balances = useUserDepositAssetBalances()
+  const depositedList: DepositAsset[] = []
+  balances?.data?.map(value => {
+    if (value.balance.isZero()) {
+      depositedList.push(value)
+    }
+  })
+  
+  console.log(">>>>", depositedList)
 
   const handleSetActiveValue = (value: ("All" | "Stable Coins")) => {
     setActiveValue(value)
   }
+  
+
   return (
     <div>
       <DepositLayout
         activeValue={activeValue}
         setActiveValue={(value) => {handleSetActiveValue(value)}}
+        depositedList={depositedList}
       />
     </div>
   );
