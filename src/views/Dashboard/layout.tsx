f15dd9c79fa3dc49c031d1996f2f23e700b73ecf
Fabian Molina <famole@gmail.com>
2021-06-24 23:00:59 -0300

    Updated dashboard ui including empty states, also added modal message

--- src/views/Dashboard/layout.tsx
+++ src/views/Dashboard/layout.tsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
   Center,
   Flex,
   Modal,
+  ModalFooter,
+  ModalHeader,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
   StackDivider,
   Text,
   VStack,
+  ModalBody,
 } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { fontSizes, spacings } from "../../utils/constants";
 import { CenterProps, HStack } from "@chakra-ui/layout";
 import { isMobileOnly } from 'react-device-detect'
 import { ModalIcon } from "../../utils/icons";
 import DashboardTable, { DashboardTableType } from "./DashboardTable";
+import { DashboardEmptyState } from './DasboardEmptyState'
+import { DepositAsset } from "../Deposit"
+import { useHistory } from "react-router-dom";
 
 interface DashboardProps {
   balance: string | undefined;
   borrowed: string | undefined;
   collateral: string | undefined;
   healthFactor: string | undefined;
+  deposits: DepositAsset[];
+};
+
+const MODAL_TYPES = {
+  AB: "AB",
+  HF: "HF",
 };
 
 export const DashboardBanner: React.FC<{}> = () => {
@@ -82,8 +93,84 @@ const UpperBox: React.FC<{title: string;} & CenterProps> = ({
   );
 };
 
+const ModalComponent: React.FC<{isOpen: boolean; mtype: string; onClose(): void;}> = ({
+  children: modalChildren,
+  mtype,
+  isOpen,
+  onClose,
+}) => {
+  return (
+    <>
+      {isOpen ? (
+        <Box
+        w="100%"
+        justifyContent="space-between"
+        px={{ base: "1.1rem", md: "2.2rem" }}
+        py={{ base: spacings.md, md: "1.9rem" }}
+        bg="secondary.900"
+        rounded="2xl"
+        position="relative"
+        minH="14.4rem"
+        minW="40%"
+        m="0.5em"
+        align="center"
+      >
+        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+          <ModalOverlay />
+          <ModalContent
+            color="primary.900"
+            background="linear-gradient(#F3FFF7, #DCFFF1)"
+            px={{ base: "1.5rem", md: "2.9rem" }}
+            py="3.5rem"
+            rounded="lg"
+            minW={{ base: "80%", md: "30vw" }}
+            minH={{ base: "50%", md: "30vh" }}
+          >
+            {mtype === MODAL_TYPES.AB && (
+              <>
+                <ModalHeader fontSize="1.6rem" fontWeight="bold">
+                  Aproximate Balance
+                </ModalHeader>
+                <ModalBody>
+                  <Text fontSize="1.4rem">
+                    Your aggregated balance shows the approximate value in USD of all the assets you have deposited. 
+                    It fluctuates based on the evolution of prices.
+                  </Text>
+                  <Text mt={5} fontSize="1.4rem">
+                    Please note that if your deposits consist of stable-coins the approximate balance in USD could 
+                    be slightly higher or lower than the stable-coin balance, due to fluctuations in the stable-coin peg.
+                  </Text>
+                </ModalBody>
+              </>
+            )}
+            <ModalFooter>
+              <Button
+                w={{ base: "100%", md: "60%" }}
+                m="auto"
+                mt={5}
+                py="1.5rem"
+                fontSize={{ base: "1.6rem", md: fontSizes.md }}
+                bg="secondary.100"
+                color="white"
+                fontWeight="normal"
+                onClick={onClose}
+              >
+                Ok, I got it
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
+      ) : (
+        null
+      )}
+    </>
+  );
+};
+
 export const DashboardLayout: React.FC<DashboardProps> = props => {
-  
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modal_type, setModal] = useState(MODAL_TYPES.AB)
   return (
     <Flex
       flexDirection="column"
@@ -97,7 +184,10 @@ export const DashboardLayout: React.FC<DashboardProps> = props => {
         m="auto"
         color="white"
       >
-        <UpperBox title="Deposit Information" mr="2rem">
+        <UpperBox 
+          title="Deposit Information"
+          mr={{base: "inherit", lg: "2rem"}}
+        >
           <VStack flexDirection="column" h="7.5rem" alignItems="baseline">
             <HStack d="flex" mt="0.5rem">
               <Text>Aproximate Balance</Text>
@@ -107,13 +197,16 @@ export const DashboardLayout: React.FC<DashboardProps> = props => {
                 right="0"
                 ml="0.5rem"
                 transform="scale(0.75)"
-                onOpen={()=>{}}
+                onOpen={onOpen}
                 />
             </HStack>  
             <Text fontWeight="bold" textAlign="left" mt="0.5em">{props.balance ?? "-"}</Text>
           </VStack>
         </UpperBox>
-        <UpperBox title="Borrow Information">
+        <UpperBox 
+          title="Borrow Information"
+          mt={{base: "2rem", lg: "inherit"}}
+        >
           <Box 
             d="flex"
             flexDirection="row"
@@ -138,7 +231,7 @@ export const DashboardLayout: React.FC<DashboardProps> = props => {
                   right="0"
                   ml="0.5rem"
                   transform="scale(0.75)"
-                  onOpen={()=>{}}
+                  onOpen={onOpen}
                   />
               </HStack>  
               <Text fontWeight="bold" textAlign="left" mt="0.5em">{props.healthFactor ?? "-"}</Text>
@@ -146,10 +239,19 @@ export const DashboardLayout: React.FC<DashboardProps> = props => {
             </Box>
         </UpperBox>
       </Flex>
-      <HStack spacing={8}>
-        <DashboardTable mode={DashboardTableType.Deposit}/>
-        <DashboardTable mode={DashboardTableType.Borrow}/>
+      <HStack spacing="0.2rem" mt="2rem">
+        {props.deposits?.length > 0 ? (
+          <DashboardTable mode={DashboardTableType.Deposit}/>
+        ) :(
+          <DashboardEmptyState type="Deposit"/>
+        )}
+        <DashboardEmptyState type="Borrow"/>
       </HStack>
+      <ModalComponent
+        isOpen={isOpen}
+        mtype={modal_type}
+        onClose={onClose}
+      />
     </Flex>
   );
 };
