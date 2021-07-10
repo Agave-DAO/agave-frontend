0f3948235cef1a00924a6cf0b8e3544cb78e7e0a
Oliver Nordbjerg <hi@notbjerg.me>
2021-07-10 02:28:48 +0200

    Pass borrow information to dashboard

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { DashboardLayout } from "./layout";
-import { useUserDepositAssetBalancesWithReserveInfo } from "../../queries/userAssets";
+import {
+  useUserDepositAssetBalancesWithReserveInfo,
+  useUserVariableDebtTokenBalances
+} from "../../queries/userAssets";
 import { BigNumber } from "ethers";
 import { ReserveTokenDefinition } from "../../queries/allReserveTokens";
 
@@ -12,17 +15,24 @@ export interface AssetData {
 }
 
 export const Dashboard: React.FC<{}> = () => {
+  const borrows = useUserVariableDebtTokenBalances();
+  const borrowedList: AssetData[] = React.useMemo(
+    () => (borrows?.data?.filter(asset => !asset.balance.isZero()) ?? []),
+    [borrows]
+  );
+  console.log('borrows', borrows)
+
   const balances = useUserDepositAssetBalancesWithReserveInfo();
   const depositedList: AssetData[] = React.useMemo(
     () => (balances?.data?.filter(asset => !asset.balance.isZero()) ?? []).map(a => ({ ...a, backingReserve: a.reserve })),
     [balances]
   );
-  
+ 
   return (
     <DashboardLayout
       borrowed={undefined}
       collateral={undefined}
-      borrows={undefined}
+      borrows={borrowedList}
       deposits={depositedList}
       healthFactor={undefined}
     />
