1c449b029f327af3d5bfcb9bee4a230ea3f0d246
Affinity-Design <paul@affinitydesign.ca>
2021-07-29 13:05:43 -0400

    navbar overhaul

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,13 @@
-import { useMemo, useState } from "react";
-import { Text, Center, Button, Badge } from "@chakra-ui/react";
+import React, { useMemo, useState } from "react";
+import {
+  Text,
+  Center,
+  Button,
+  Badge,
+  Image,
+  useColorMode,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
 import { Navbar } from "./navbar";
 import agaveLogo from "../../assets/image/agave-logo.svg";
 import darkMoon from "../../assets/image/dark-moon.svg";
@@ -8,11 +16,19 @@ import { selectAddress } from "../../features/auth/authSlice";
 import { useAppSelector } from "../../redux/hooks";
 import { NavLink } from "react-router-dom";
 import { NavTabLink } from "./tab-link";
-import { fontSizes } from "../../utils/constants";
+import { bigNumberToString } from "../../utils/fixedPoint";
+import { useUserAssetBalance } from "../../queries/userAssets";
+import { useAllReserveTokensWithData } from "../../queries/lendingReserveData";
+import { store as NotificationManager } from "react-notifications-component";
+import { useWeb3React } from "@web3-react/core";
+import { injectedConnector } from "../../hooks/injectedConnectors";
 
 function Header() {
-  // eslint-disable-next-line
+  // Light/Dark button functions
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // Address button functions
   const address: string | undefined = useAppSelector(selectAddress);
   const addressPretty = useMemo(
     () =>
@@ -25,83 +41,133 @@ function Header() {
     [address]
   );
 
+  // Agve button functions
+  const reserves = useAllReserveTokensWithData()?.data;
+  const reserve = React.useMemo(
+    () =>
+      reserves?.find(reserve => reserve?.symbol === "AGVE") ??
+      reserves?.find(reserve => reserve.tokenAddress),
+    [reserves]
+  );
+  const tokenBalance = useUserAssetBalance(reserve?.tokenAddress)?.data;
+  const userBal = tokenBalance ? bigNumberToString(tokenBalance) : "0";
+
+  // Connect button functions
+  function warnUser(title: string, message?: string | undefined): void {
+    NotificationManager.addNotification({
+      container: "top-right",
+      type: "warning",
+      title,
+      message,
+    });
+  }
+  const { activate, error } = useWeb3React();
+  const onMetamaskConnect = async () => {
+    if (typeof (window as any).ethereum === "undefined") {
+      warnUser(
+        "Please install MetaMask!",
+        "Agave requires Metamask to be installed in your browser to work properly."
+      );
+      return;
+    }
+    await activate(injectedConnector);
+  };
+
   return (
     <Navbar>
       <Navbar.Brand>
         <Center as={NavLink} to="/" marginEnd={6}>
-          <img src={agaveLogo} alt="AGAVE ALT" />
-          <Text color="white" ml={4} fontWeight="bold">
+          <Image
+            src={agaveLogo}
+            alt="AGAVE ALT"
+            width={{ base: "35px", md: "inherit" }}
+          />
+          <Text
+            color="white"
+            ml={4}
+            mt={{ base: "2.5", md: "1" }}
+            fontWeight="bold"
+            fontSize={{ base: "4xl", md: "2xl" }}
+          >
             AGAVE
           </Text>
         </Center>
       </Navbar.Brand>
       <Navbar.Links>
-        <NavTabLink w="14rem" exact to="/dashboard" fontWeight="bold">
+        <NavTabLink exact to="/dashboard">
           DASHBOARD
         </NavTabLink>
-        <NavTabLink exact to="/markets" fontWeight="bold">
+        <NavTabLink exact to="/markets">
           MARKETS
         </NavTabLink>
-        <NavTabLink exact to="/borrow" fontWeight="bold">
+        <NavTabLink exact to="/borrow">
           BORROW
         </NavTabLink>
-        <NavTabLink exact to="/deposit" fontWeight="bold">
+        <NavTabLink exact to="/deposit">
           DEPOSIT
         </NavTabLink>
-        <NavTabLink exact to="/stake" fontWeight="bold">
+        <NavTabLink exact to="/stake">
           STAKE
         </NavTabLink>
       </Navbar.Links>
       <Navbar.UserProfile>
         <Center
-          width="3rem"
-          height="3rem"
+          width={{ base: "4rem", md: "3rem" }}
+          height={{ base: "4rem", md: "3rem" }}
           rounded="lg"
-          bg="primary.500"
+          bg={mode({ base: "primary.800", md: "primary.500" }, "primary.500")}
           cursor="pointer"
-          display="none"
+          // display="none"
+          onClick={toggleColorMode}
         >
-          <img src={isDarkMode ? darkMoon : lightMoon} alt="theme-mode" />
+          <Image
+            src={colorMode === "dark" ? darkMoon : lightMoon}
+            alt="theme-mode"
+          />
         </Center>
         <Center
           minWidth="10rem"
-          height="3rem"
-          fontSize={fontSizes.md}
+          height={{ base: "4rem", md: "3rem" }}
+          fontSize={{ base: "3xl", md: "2xl" }}
           mx="1.5rem"
           textTransform="uppercase"
           color="white"
-          bg="primary.500"
+          bg={mode({ base: "secondary.800", md: "primary.500" }, "primary.500")}
           rounded="lg"
-          display="none"
         >
-          0 AGVE
+          {userBal.substring(0, 4)} AGVE
         </Center>
         {addressPretty ? (
           <Center
-            background="primary.500"
+            background={mode(
+              { base: "secondary.800", md: "primary.500" },
+              "primary.500"
+            )}
             rounded="lg"
             minWidth="10rem"
-            height="3rem"
+            height={{ base: "4rem", md: "3rem" }}
             color="white"
+            p="10px"
           >
             <Badge
               bg="yellow"
               rounded="full"
-              width="1rem"
-              height="1rem"
+              width={{ base: "1.3rem", md: "1rem" }}
+              height={{ base: "1.3rem", md: "1rem" }}
               mr="5px"
             />
-            <Text fontSize={fontSizes.md}>{addressPretty}</Text>
+            <Text fontSize={{ base: "3xl", md: "2xl" }}>{addressPretty}</Text>
           </Center>
         ) : (
           <Button
             background="primary.500"
             rounded="lg"
             minWidth="10rem"
-            height="3rem"
-            fontSize={fontSizes.md}
+            height={{ base: "4rem", md: "3rem" }}
+            fontSize={{ base: "3xl", md: "2xl" }}
             fontWeight="normal"
             color="white"
+            onClick={onMetamaskConnect}
           >
             Connect
           </Button>
--- src/components/Header/navbar.tsx
+++ src/components/Header/navbar.tsx
@@ -1,76 +1,240 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, ChevronLeftIcon } from "@chakra-ui/icons";
+import glowingAgave from "../../assets/image/glowing-agave.svg";
 import {
+  Center,
+  Image,
   Flex,
   HStack,
   IconButton,
-  Spacer,
-  Stack,
   TabList,
   Tabs,
   Divider,
   useColorModeValue as mode,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { isValidElement, ReactElement } from "react";
+import React, {
+  isValidElement,
+  ReactElement,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 import { MobileNavContent } from "./mobile-nav";
+import { useHistory } from "react-router-dom";
 
 export const Template: React.FC = props => {
   const children = React.Children.toArray(props.children).filter<ReactElement>(
     isValidElement
   );
   const mobileNav = useDisclosure();
+  const history = useHistory();
+  const [boxWidth, setBoxWidth] = useState(0);
+
+  useEffect(() => {
+    const w = document.getElementsByClassName("profile");
+    setBoxWidth(w[0].clientWidth ? w[0].clientWidth - 110 : 0);
+  }, [boxWidth]);
+
   return (
-    <Flex
-      py={4}
-      px={{ base: 4, md: 6, lg: 8 }}
-      boxShadow="none"
-      borderBottomWidth={mode("0", "1px")}
-      bg={mode("primary.900", "secondary.900")}
-      minH="4.8rem"
-    >
-      {children.find(child => child.type === Brand)?.props.children}
-      <Spacer />
-      <HStack display={{ base: "none", md: "flex" }} marginStart={4}>
-        <Tabs colorScheme="blue" variant="unstyled" isFitted>
-          <TabList>
+    <React.Fragment>
+      <Flex
+        zIndex="1"
+        className="NavBar"
+        bg={mode("primary.900", "secondary.900")}
+        boxShadow="none"
+        justifyContent="space-between"
+        alignItems="center"
+        p="0px 30px"
+        h={{ base: "80px", md: "50px" }}
+        top="0"
+        wrap="wrap"
+        position="static"
+        borderBottomColor={"primary.50"}
+        borderBottomWidth="1px"
+        overflowY="hidden"
+      >
+        <IconButton
+          display={{ base: "inherit", md: "none" }}
+          size="lg"
+          w="40px"
+          h="40px"
+          fontSize="30px"
+          aria-label="Back"
+          variant="outline"
+          onClick={() => history.goBack()}
+          colorScheme="whiteAlpha"
+          color="white"
+          icon={<ChevronLeftIcon fontSize="35px" onClick={mobileNav.onClose} />}
+          zIndex={3}
+        />
+        <Flex zIndex={3}>
+          {children.find(child => child.type === Brand)?.props.children}
+        </Flex>
+
+        {/* Add spacer to off center tabs to the right */}
+        {/* <Spacer /> */}
+        <Tabs
+          variant="unstyled"
+          className="Tabs"
+          display={{ base: "none", md: "flex" }}
+          h="100%"
+          ml={{ base: "0", md: "1", lg: boxWidth }}
+          justifyContent="center"
+          alignItems="center"
+          zIndex={3}
+        >
+          <TabList className="TabList" display="flex" h="100%" flex="1 1 auto">
             {children.find(child => child.type === Links)?.props.children}
           </TabList>
         </Tabs>
-      </HStack>
 
-      <HStack display={{ base: "none", md: "flex" }} spacing={3} ml="4rem">
-        {children.find(child => child.type === UserProfile)?.props.children}
-      </HStack>
+        <HStack
+          className="profile"
+          display={{ base: "none", md: "flex" }}
+          spacing={3}
+          zIndex={3}
+        >
+          {children.find(child => child.type === UserProfile)?.props.children}
+        </HStack>
 
-      <IconButton
-        display={{ base: "flex", md: "none" }}
-        size="sm"
-        aria-label="Open menu"
-        fontSize="20px"
-        variant="ghost"
-        onClick={mobileNav.onOpen}
-        color="white"
-        icon={<HamburgerIcon />}
-      />
+        <IconButton
+          display={{ base: "flex", md: "none" }}
+          size="lg"
+          aria-label="Open menu"
+          fontSize="30px"
+          w="40px"
+          h="40px"
+          variant="outline"
+          onClick={mobileNav.isOpen ? mobileNav.onClose : mobileNav.onOpen}
+          color="white"
+          icon={<HamburgerIcon fontSize="28px" />}
+          colorScheme="whiteAlpha"
+          _activeLink={{ boxShadow: "black" }}
+          zIndex={3}
+        />
 
-      <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose}>
-        <Stack spacing={5}>
-          <Flex>
-            {children.find(child => child.type === Brand)?.props.children}
+        <MobileNavContent isOpen={mobileNav.isOpen} onClose={mobileNav.onClose}>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Center
+              pos="relative"
+              mt={{ base: "-80px", md: "-50px" }}
+              width="100%"
+              overflowY="hidden"
+              overflowX="hidden"
+              h={{ base: "80px", md: "50px" }}
+              maxH={{ base: "80px", md: "50px" }}
+              zIndex="0"
+              top="80px"
+            >
+              <Image
+                pos="absolute"
+                src={glowingAgave}
+                alt="glowing agave log"
+                top={{ base: "-5rem", md: "-15rem", lg: "-25rem" }}
+                left="0"
+              />
+            </Center>
+            <Flex
+              className="NavBar2"
+              bg={mode("primary.900", "secondary.900")}
+              boxShadow="none"
+              justifyContent="space-between"
+              alignItems="center"
+              p="0px 30px"
+              h={{ base: "80px", md: "50px" }}
+              top="0"
+              w="100%"
+              position="static"
+              borderBottomColor={"primary.50"}
+              borderBottomWidth="1px"
+              overflowY="hidden"
+            >
+              <IconButton
+                display={{ base: "inherit", md: "none" }}
+                size="lg"
+                w="40px"
+                h="40px"
+                fontSize="30px"
+                aria-label="Back"
+                variant="outline"
+                onClick={() => history.goBack()}
+                colorScheme="whiteAlpha"
+                color="white"
+                icon={
+                  <ChevronLeftIcon
+                    fontSize="35px"
+                    onClick={mobileNav.onClose}
+                  />
+                }
+                zIndex={3}
+              />
+              <Flex zIndex={3}>
+                {children.find(child => child.type === Brand)?.props.children}
+              </Flex>
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                size="lg"
+                aria-label="Open menu"
+                fontSize="30px"
+                w="40px"
+                h="40px"
+                // mr="11"
+                variant="outline"
+                onClick={
+                  mobileNav.isOpen ? mobileNav.onClose : mobileNav.onOpen
+                }
+                color="white"
+                icon={<HamburgerIcon fontSize="28px" />}
+                colorScheme="whiteAlpha"
+                _activeLink={{ boxShadow: "black" }}
+                zIndex={3}
+              />
+            </Flex>
+            <Flex
+              h="10rem"
+              w="100%"
+              justifyContent="center"
+              alignItems="center"
+            >
+              {
+                children.find(child => child.type === UserProfile)?.props
+                  .children
+              }
+            </Flex>
+            <Divider />
+            <Flex>
+              <Tabs orientation="vertical" variant="unstyled">
+                <TabList onClick={mobileNav.onClose} h="100%" w="100%">
+                  {children.find(child => child.type === Links)?.props.children}
+                </TabList>
+              </Tabs>
+            </Flex>
           </Flex>
-          <Tabs orientation="vertical" variant="unstyled">
-            <TabList>
-              {children.find(child => child.type === Links)?.props.children}
-            </TabList>
-          </Tabs>
-          <Divider />
-
-          <Flex>
-            {children.find(child => child.type === UserProfile)?.props.children}
-          </Flex>
-        </Stack>
-      </MobileNavContent>
-    </Flex>
+        </MobileNavContent>
+      </Flex>
+      <Center
+        pos="relative"
+        mt={{ base: "-80px", md: "-50px" }}
+        width="100%"
+        overflowY="hidden"
+        overflowX="hidden"
+        h={{ base: "80px", md: "50px" }}
+        maxH={{ base: "80px", md: "50px" }}
+        zIndex="0"
+      >
+        <Image
+          pos="absolute"
+          src={glowingAgave}
+          alt="glowing agave log"
+          top={{ base: "-5rem", md: "-15rem", lg: "-25rem" }}
+          left="0"
+        />
+      </Center>
+    </React.Fragment>
   );
 };
 
@@ -78,4 +242,8 @@ const Brand: React.FC = () => null;
 const Links: React.FC = () => null;
 const UserProfile: React.FC = () => null;
 
-export const Navbar = Object.assign(Template, { Brand, Links, UserProfile });
+export const Navbar = Object.assign(Template, {
+  Brand,
+  Links,
+  UserProfile,
+});
