e860eb0e5179a74e912502c440531c3e4c769b24
luigy-lemon <luigy-lemon@protonmail.com>
2021-09-24 10:17:01 +0100

    fix for when repaying an amount lower than the minimum reserve amount

--- src/views/Repay/RepayDetail.tsx
+++ src/views/Repay/RepayDetail.tsx
@@ -103,7 +103,7 @@ const InitialComp: React.FC<{
     // (difference gets minted as agToken)
     // TODO: Change this to a time-based multiplier of APR, for example, 10 minutes worth of interest
     maxRepaymentAmount = maxRepaymentAmount.gt(0)
-      ? maxRepaymentAmount.mul(2001).div(2000)
+      ? maxRepaymentAmount.add(MINIMUM_NATIVE_RESERVE).mul(2001).div(2000)
       : maxRepaymentAmount;
     // Max repayment amount cannot be negative
     maxRepaymentAmount = bnMin(maxRepaymentAmount, userBalance);
