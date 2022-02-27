4de1498848f162d3bc6b990cd49f7806051e69b7
Mariano Shaar <a00825287@itesm.mx>
2022-02-26 21:05:22 -0600

    Added wrapper for tabTable

---
+++ src/views/Dashboard/tabContent.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  Center,
+  Button,
+  TabPanels, 
+  TabPanel,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  Input,
+  InputGroup,
+  InputRightElement,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { CenterProps } from "@chakra-ui/layout";
+import { TokenIcon } from "../../utils/icons";
+import { fontSizes } from "../../utils/constants";
+import { TabTable } from "./tabTable";
+
+export const TabContent: React.FC<{
+    type: string;
+    coin: string;
+    props?: CenterProps;
+
+  }> = ({ type, coin, ...props }) => {
+    return (
+        <>
+        <TabPanels border='1px' borderColor='white'>
+          <TabPanel>
+            <TabTable 
+              type={type}
+              coin={coin}
+            />
+          </TabPanel>
+          <TabPanel>
+          <TabTable 
+              type={type}
+              coin={coin}
+            />
+          </TabPanel>
+        </TabPanels>
+        <Center mb='1em'>
+          <Button
+            background="linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);"
+            color="secondary.900"
+            px={{ base: "10rem", md: "6rem" }}
+            py="1.5rem"
+            fontSize={fontSizes.md}
+            mt="2rem"
+          >{type}</Button>
+        </Center>
+        </>
+    );
+  };
+  
