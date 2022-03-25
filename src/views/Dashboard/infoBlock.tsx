6e46c58bd2ae056c72f0b0625674d5a44abe038c
Rodrigo Casale <rodrigocasale@Rodrigos-MacBook-Pro.local>
2022-03-25 12:50:20 -0600

    ran prettier in Info Block related files

--- src/views/Dashboard/infoBlock.tsx
+++ src/views/Dashboard/infoBlock.tsx
@@ -7,12 +7,12 @@ import {
   VStack,
   Spinner,
   Select,
-  Tabs, 
-  TabList,  
-  Tab, 
+  Tabs,
+  TabList,
+  Tab,
 } from "@chakra-ui/react";
 import { CenterProps } from "@chakra-ui/layout";
-import { TabContent } from "./tabContent"
+import { TabContent } from "./tabContent";
 import { useAllReserveTokensWithData } from "../../queries/lendingReserveData";
 import { useNativeSymbols } from "../../utils/icons";
 
@@ -23,146 +23,151 @@ interface AssetRecord {
 }
 
 const LowerBox: React.FC<{ title: string } & CenterProps> = ({
-    title,
-    children,
-    ...props
-  }) => {
-    return (
-      <Center
-        boxSizing="content-box"
+  title,
+  children,
+  ...props
+}) => {
+  return (
+    <Center
+      boxSizing="content-box"
+      flexDirection="column"
+      rounded="xl"
+      minH="10.6rem"
+      minW={{ base: "100%", lg: "auto" }}
+      bg="primary.900"
+      py="1rem"
+      {...props}
+    >
+      <VStack
+        divider={
+          <StackDivider
+            borderColor="#36CFA2"
+            h="0.188rem"
+            backgroundColor="#36CFA2"
+          />
+        }
+        spacing={4}
+        w="100%"
+        align="stretch"
         flexDirection="column"
-        rounded="xl"
-        minH="10.6rem"
-        minW={{ base: "100%", lg: "auto" }}
-        bg="primary.900"
-        py="1rem"
-        {...props}
       >
-        <VStack
-          divider={
-            <StackDivider
-              borderColor="#36CFA2"
-              h="0.188rem"
-              backgroundColor="#36CFA2"
-            />
-          }
-          spacing={4}
-          w="100%"
-          align="stretch"
-          flexDirection="column"
-        >
-          <Text px={{ base: "2rem", md: "3rem" }} h="25">
-            {title}
-          </Text>
-          <Box px={{ base: "2rem", md: "3rem" }}>{children}</Box>
-        </VStack>
-      </Center>
-    );
+        <Text px={{ base: "2rem", md: "3rem" }} h="25">
+          {title}
+        </Text>
+        <Box px={{ base: "2rem", md: "3rem" }}>{children}</Box>
+      </VStack>
+    </Center>
+  );
 };
 
 export const InfoBlock: React.FC<{
-    type: string;
-    props?: CenterProps;
-
+  type: string;
+  props?: CenterProps;
 }> = ({ type, ...props }) => {
-  const [selectedTab, setTab] = useState(type === "Deposit" ? "Deposit" : "Withdraw");
+  const [selectedTab, setTab] = useState(
+    type === "Deposit" ? "Deposit" : "Withdraw"
+  );
   const [selectedCoin, setCoin] = useState("USDC"); // defaults to USDC since it's the first coin on the list
   const handleCoinChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setCoin(event.target.value)
-  }
+    setCoin(event.target.value);
+  };
 
   const reserves = useAllReserveTokensWithData();
   const nativeSymbols = useNativeSymbols();
   const assetRecords = React.useMemo(() => {
-  const assets =
-    reserves.data?.map(
-      ({ symbol, tokenAddress, aTokenAddress }): AssetRecord => ({
-        symbol,
-        tokenAddress,
-        aTokenAddress,
-      })
-    ) ?? [];
-  return assets.map(asset => {
-    return asset.symbol === nativeSymbols.wrappednative
-      ? {
-          ...asset,
-          symbol: nativeSymbols?.native,
-        }
-      : asset;
+    const assets =
+      reserves.data?.map(
+        ({ symbol, tokenAddress, aTokenAddress }): AssetRecord => ({
+          symbol,
+          tokenAddress,
+          aTokenAddress,
+        })
+      ) ?? [];
+    return assets.map(asset => {
+      return asset.symbol === nativeSymbols.wrappednative
+        ? {
+            ...asset,
+            symbol: nativeSymbols?.native,
+          }
+        : asset;
     });
   }, [reserves]);
 
   const coinOptions = React.useMemo(
     () =>
-      assetRecords.map( currency => {
-        return (
-            <option value={currency.symbol}>{currency.symbol}</option>
-        )
-      })
-    , [assetRecords]
+      assetRecords.map(currency => {
+        return <option value={currency.symbol}>{currency.symbol}</option>;
+      }),
+    [assetRecords]
   );
   return (
     <LowerBox
       title={type}
-      mr={type === "Deposit" ? ({ base: "0", lg: "1%" }) : "0" }
-      ml={type === "Borrow" ? ({ base: "0", lg: "1%" }) : "0" }
-      color='white'
-      mb='1em'
+      mr={type === "Deposit" ? { base: "0", lg: "1%" } : "0"}
+      ml={type === "Borrow" ? { base: "0", lg: "1%" } : "0"}
+      color="white"
+      mb="1em"
       minW={{ base: "100%", lg: "49%" }}
     >
-      {(!coinOptions.length) 
-        ? (
-          <Center>
-            <Spinner 
-              speed="0.5s" 
-              emptyColor="gray.200" 
-              color="yellow.500" 
-              size='xl' />
-          </Center>
-          )
-        : (
-          <>
-          <Select 
-            borderColor='#00A490'
-            bg='#00A490'
-            size='lg'
-            color='white'
-            mb='2em'
-            mt='1em'
+      {!coinOptions.length ? (
+        <Center width={"100%"}>
+          <Spinner
+            speed="0.5s"
+            emptyColor="gray.200"
+            color="yellow.500"
+            size="xl"
+          />
+        </Center>
+      ) : (
+        <>
+          <Select
+            borderColor="#00A490"
+            bg="#00A490"
+            size="lg"
+            color="white"
+            mb="2em"
+            mt="1em"
             onChange={handleCoinChange}
           >
             {coinOptions}
           </Select>
-          <Tabs 
-            isFitted 
-            variant='enclosed'
-            onChange={(index) => {
+          <Tabs
+            isFitted
+            variant="enclosed"
+            onChange={index => {
               if (type === "Deposit") {
-                setTab(index ? "Withdraw" : "Deposit")
+                setTab(index ? "Withdraw" : "Deposit");
               } else {
-                setTab(index ? "Repay" : "Borrow")
+                setTab(index ? "Repay" : "Borrow");
               }
             }}
           >
             <TabList>
               <Tab
-                fontSize='17'
-                _selected={{ color: '#044D44', background: "linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);" }}
-              > {type === "Deposit" ? "Deposit" : "Borrow"} </Tab>
-              <Tab 
-                fontSize='17'
-                _selected={{ color: '#044D44', background: "linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);" }}
-              > {type === "Deposit" ? "Withdraw" : "Repay"} </Tab>
+                fontSize="17"
+                _selected={{
+                  color: "#044D44",
+                  background:
+                    "linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);",
+                }}
+              >
+                {type === "Deposit" ? "Deposit" : "Borrow"}
+              </Tab>
+              <Tab
+                fontSize="17"
+                _selected={{
+                  color: "#044D44",
+                  background:
+                    "linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);",
+                }}
+              >
+                {type === "Deposit" ? "Withdraw" : "Repay"}
+              </Tab>
             </TabList>
-          <TabContent
-            type={type}
-            tab={selectedTab}
-            coin={selectedCoin}
-          />
-        </Tabs>
+            <TabContent type={type} tab={selectedTab} coin={selectedCoin} />
+          </Tabs>
         </>
-        )
-      }
+      )}
     </LowerBox>
-  )
-}
+  );
+};
--- src/views/Dashboard/tabContent.tsx
+++ src/views/Dashboard/tabContent.tsx
@@ -1,53 +1,38 @@
 import React from "react";
-import {
-  Center,
-  Button,
-  TabPanels, 
-  TabPanel,
-} from "@chakra-ui/react";
+import { Center, Button, TabPanels, TabPanel } from "@chakra-ui/react";
 import { CenterProps } from "@chakra-ui/layout";
 import { fontSizes } from "../../utils/constants";
 import { TabTable } from "./tabTable";
 
 export const TabContent: React.FC<{
-    type: string;
-    tab: string;
-    coin: string;
-    props?: CenterProps;
-
-  }> = ({ type, tab, coin, ...props }) => {
-    return (
-        <>
-        <TabPanels border='1px' borderColor='white'>
-          <TabPanel>
-            <TabTable 
-              type={type}
-              tab={tab}
-              coin={coin}
-            />
-          </TabPanel>
-          <TabPanel>
-            <TabTable 
-              type={type}
-              tab={tab}
-              coin={coin}
-            />
-          </TabPanel>
-        </TabPanels>
-        <Center 
-          mb='1em'
+  type: string;
+  tab: string;
+  coin: string;
+  props?: CenterProps;
+}> = ({ type, tab, coin, ...props }) => {
+  return (
+    <>
+      <TabPanels border="1px" borderColor="white">
+        <TabPanel>
+          <TabTable type={type} tab={tab} coin={coin} />
+        </TabPanel>
+        <TabPanel>
+          <TabTable type={type} tab={tab} coin={coin} />
+        </TabPanel>
+      </TabPanels>
+      <Center mb="1em">
+        <Button
+          background="linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);"
+          color="secondary.900"
+          px={{ base: "10rem", md: "8rem" }}
+          py="1.5rem"
+          fontSize={fontSizes.md}
+          mt="2rem"
+          w="30%"
         >
-          <Button
-            background="linear-gradient(90.53deg, #9BEFD7 0%, #8BF7AB 47.4%, #FFD465 100%);"
-            color="secondary.900"
-            px={{ base: "10rem", md: "8rem" }}
-            py="1.5rem"
-            fontSize={fontSizes.md}
-            mt="2rem"
-            w="30%"
-          >{tab}</Button>
-        </Center>
-        </>
-    );
-  };
-  
+          {tab}
+        </Button>
+      </Center>
+    </>
+  );
+};
